8b3e34253f2a30900a420668411cccebadc1012e
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2019-10-05 23:05:19 +0300

    Removed SeekBar

--- SmartRadiatorAndroid/app/src/main/java/com/example/smartradiator/MainActivity.java
+++ SmartRadiatorAndroid/app/src/main/java/com/example/smartradiator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.smartradiator;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
@@ -19,13 +20,12 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     String CITY = "ruse,bg";
     String API = "18143c937ebb4feb9e6bdd0726a4ee8e";
-    TextView addressTxt, updated_atTxt, statusTxt, tempTxt;
+    TextView addressTxt, updated_atTxt, tempTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         addressTxt = findViewById(R.id.address);
         updated_atTxt = findViewById(R.id.updated_at);
         tempTxt = findViewById(R.id.temp);
@@ -61,7 +61,13 @@ public class MainActivity extends AppCompatActivity {
 
                 Long updatedAt = jsonObj.getLong("dt");
                 String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
-                String temp = main.getString("temp") + "°C";
+                double a = Double.parseDouble(main.getString("temp"));
+                Math.round(a);
+                Integer temp = (int) a;
+                String newTemp = String.valueOf(temp);
+
+
+
 
 
                 String address = jsonObj.getString("name") + ", " + sys.getString("country");
@@ -70,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
                 /* Populating extracted data into our views */
                 addressTxt.setText(address);
                 updated_atTxt.setText(updatedAtText);
-                tempTxt.setText(temp);
+                tempTxt.setText(newTemp);
 
                 /* Views populated, Hiding the loader, Showing the main design */
                 findViewById(R.id.loader).setVisibility(View.GONE);
